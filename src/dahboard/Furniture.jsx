77697227d7ad4c45fa1a0c54ca2c8f95cf4b1fe8
Kavita Gairola <kavitagairola507@gmail.com>
2025-06-25 13:57:23 +0530

    banner created

--- src/dahboard/Furniture.jsx
+++ src/dahboard/Furniture.jsx
@@ -1,17 +1,27 @@
 function Furniture() {
   return (
     <>
-      <div
-        className="w-[90vw] h-[60vh] bg-no-repeat bg-cover bg-center flex  "
-        style={{
-          backgroundImage:
-            "url('https://avon-demo.myshopify.com/cdn/shop/files/chair-bnr_1296x.jpg?v=1614331382')",
-            
-        }}
-      >
-        <h1 className="text-white text-3xl md:text-5xl font-bold">
-          Kavita
-        </h1>
+     
+      <div >
+        <div
+          className="h-[60vh] w-[85vw] bg-no-repeat bg-cover bg-center"
+          style={{
+            backgroundImage:
+              "url('https://avon-demo.myshopify.com/cdn/shop/files/chair-bnr_1296x.jpg?v=1614331382')",
+          }}
+        >
+          
+          <div className="max-w-7xl absolute h-full px-6 sm:px-10 md:px-20 lg:px-40 mt-44">
+            <p className="text-sm sm:text-base mb-2">It’s for grand living</p>
+            <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold leading-snug">
+              Different Furniture,
+            </h2>
+            <h2 className="text-2xl sm:text-3xl md:text-4xl font-semibold leading-snug">
+              Different Stories
+            </h2>
+            <button className="bg-slate-950 text-white mt-4">BUY NOW</button>
+          </div>
+        </div>
       </div>
     </>
   );
